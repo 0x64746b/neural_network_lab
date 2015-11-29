b9edafa531d43f27536306b9d17780058f18cf6c
dtk <dtk@gmx.de>
2015-11-29 16:37:48 +0100

    Turn function approximation into generation

--- sine/mlp.py
+++ sine/mlp.py
@@ -56,7 +56,7 @@ class RecurrentLayer(Layer):
     def __init__(self, dimension, input_dimension, transfer_func):
         self.recurrent_weights = np.random.uniform(-1.0, 1.0, (dimension, dimension))
         self._previous_output = np.zeros(dimension)
-        
+
         super(RecurrentLayer, self).__init__(dimension, input_dimension, transfer_func)
 
     def process(self, input_vector):
@@ -74,29 +74,28 @@ class RecurrentLayer(Layer):
 
 
 if __name__ == '__main__':
-    input_data = np.linspace(0, 2*math.pi, num=NUM_SAMPLES, endpoint=False).reshape((NUM_SAMPLES, 1))
-    expected_outputs = np.array([math.sin(x) for x in input_data]).reshape((NUM_SAMPLES, 1))
+    sampling_points = np.linspace(0, 2*math.pi, num=NUM_SAMPLES, endpoint=False)
+    input_data = np.array([math.sin(x) for x in sampling_points]).reshape((NUM_SAMPLES, 1))
 
     hidden = RecurrentLayer(NUM_HIDDEN_NODES, input_data.shape[1], expit)
-    output = Layer(expected_outputs.shape[1], NUM_HIDDEN_NODES, lambda x: x)
+    output = Layer(input_data.shape[1], NUM_HIDDEN_NODES, lambda x: x)
 
     for epoch in range(100 * NUM_SAMPLES):
-        # get a random date
-        next_index = epoch % NUM_SAMPLES
+        current_index = epoch % NUM_SAMPLES
+        next_index = (current_index + 1) % NUM_SAMPLES
 
         # process inputs
-        outputs = output.process(hidden.process(input_data[next_index]))
+        outputs = output.process(hidden.process(input_data[current_index]))
 
         # calculate errors
-        output.errors = expected_outputs[next_index] - outputs
+        output.errors = input_data[next_index] - outputs
         hidden.errors = expit_prime(hidden.h) * np.dot(output.errors, output.weights)
 
         # update weights and biases
         output.update()
         hidden.update()
 
-        print('input:', input_data[next_index])
-        print('expected output:', expected_outputs[next_index])
+        print('input:', input_data[current_index])
+        print('expected output:', input_data[next_index])
         print('actual output:', outputs)
         print('output error:', output.errors)
-        print('hidden errors:', hidden.errors)
