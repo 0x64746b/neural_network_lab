ff3ce4035ffff3ee1ea3b14314dd7baa38b4249d
dtk <dtk@gmx.de>
2015-12-14 15:20:39 +0100

    Add history to `RecurrentLayer`

--- sine/mlp.py
+++ sine/mlp.py
@@ -9,6 +9,7 @@ from __future__ import (
 )
 
 
+from collections import deque
 import math
 
 import numpy as np
@@ -16,8 +17,10 @@ from scipy.special import expit
 
 # [0, max, pi, min] + 3 recursive bisections for each resulting quarter
 NUM_SAMPLES = 4 + 4 * 7
+NUM_EPOCHS = 50000 * NUM_SAMPLES
+HISTORY_LENGTH = 3
 
-NUM_HIDDEN_NODES = 5
+NUM_HIDDEN_NODES = 30
 ACCEPTED_ERROR = 1e-3
 ERROR_RATE = 0.05
 LEARNING_RATE = 0.1
@@ -53,49 +56,63 @@ class Layer(object):
 class RecurrentLayer(Layer):
     """Encapsulate the state of a recurrent layer."""
 
-    def __init__(self, dimension, input_dimension, transfer_func):
-        self.recurrent_weights = np.random.uniform(-1.0, 1.0, (dimension, dimension))
-        self._previous_output = np.zeros(dimension)
-
+    def __init__(self, dimension, input_dimension, transfer_func, history_length=None):
         super(RecurrentLayer, self).__init__(dimension, input_dimension, transfer_func)
 
+        self.recurrent_weights = np.random.uniform(-1.0, 1.0, (dimension, dimension))
+        self.input_vectors = deque(maxlen=history_length)
+        self.h = deque(maxlen=history_length)
+        self.outputs = deque(np.zeros(dimension * dimension).reshape(dimension, dimension), maxlen=history_length)
+        self.errors = deque(maxlen=history_length)
+
     def process(self, input_vector):
-        self.h = np.dot(self.weights, input_vector) + np.dot(self.recurrent_weights, self._previous_output) + self.biases
+        weighted_sum = np.dot(self.weights, input_vector) + np.dot(self.recurrent_weights, self.outputs[0]) + self.biases
+        activation = self._transfer_func(weighted_sum)
 
-        # remember output for next round
-        self._previous_output = self._transfer_func(self.h)
+        # update history
+        self.input_vectors.appendleft(input_vector)
+        self.h.appendleft(weighted_sum)
+        self.outputs.appendleft(activation)
 
-        return self._previous_output
+        return activation
 
     def update(self):
-        self.recurrent_weights = LEARNING_RATE * np.outer(self.errors, self._previous_output)
-
-        super(RecurrentLayer, self).update()
+        for index in range(len(self.errors)):
+            self.weights += LEARNING_RATE * np.outer(self.errors[index], self.input_vectors[index])
+            try:
+                self.recurrent_weights += LEARNING_RATE * np.outer(self.errors[index], self.outputs[index+1])
+            except IndexError:
+                pass
+            self.biases += LEARNING_RATE * self.errors[index]
 
 
 if __name__ == '__main__':
     sampling_points = np.linspace(0, 2*math.pi, num=NUM_SAMPLES, endpoint=False)
     input_data = np.array([math.sin(x) for x in sampling_points]).reshape((NUM_SAMPLES, 1))
 
-    hidden = RecurrentLayer(NUM_HIDDEN_NODES, input_data.shape[1], expit)
+    hidden = RecurrentLayer(NUM_HIDDEN_NODES, input_data.shape[1], expit, HISTORY_LENGTH)
     output = Layer(input_data.shape[1], NUM_HIDDEN_NODES, lambda x: x)
 
-    for epoch in range(100 * NUM_SAMPLES):
+    for epoch in range(NUM_EPOCHS):
         current_index = epoch % NUM_SAMPLES
         next_index = (current_index + 1) % NUM_SAMPLES
 
         # process inputs
         outputs = output.process(hidden.process(input_data[current_index]))
 
-        # calculate errors
+        # backpropagate errors
         output.errors = input_data[next_index] - outputs
-        hidden.errors = expit_prime(hidden.h) * np.dot(output.errors, output.weights)
 
-        # update weights and biases
+        hidden.errors.appendleft(expit_prime(hidden.h[0]) * np.dot(output.errors, output.weights))
+        for index in range(1, len(hidden.errors)):
+            hidden.errors[index] = expit_prime(hidden.h[index]) * np.dot(hidden.errors[index-1], hidden.weights)
+
+        # learn
         output.update()
         hidden.update()
 
-        print('input:', input_data[current_index])
-        print('expected output:', input_data[next_index])
-        print('actual output:', outputs)
-        print('output error:', output.errors)
+        if epoch > (NUM_EPOCHS - NUM_SAMPLES):
+            print('input:', input_data[current_index])
+            print('expected output:', input_data[next_index])
+            print('actual output:', outputs)
+            print('output error:', output.errors[0])
